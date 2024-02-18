f1d7b09c59634e38b30a30b781f1d8aa408d7716
Moyosooreoluwa <69484117+seigdev@users.noreply.github.com>
2024-02-18 16:19:58 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:finalprojectapp/main.dart';
+import 'package:finalprojectapp/pages/authentication/login.dart';
 
 void main() {
+testWidgets('LoginPAge UI Test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+        const LoginScreen()); // Replace MyApp() with the widget that contains your HomePage
 
+    // Verify that the AppBar has the correct title.
+    expect(find.byWidget(formLabel()), findsOneWidget);
+
+    // Verify that the body contains the welcome message.
+   expect(find.byWidget(textInput()), findsOneWidget);
+
+    // Verify that the FloatingActionButton is present.
+   expect(find.byWidget(submissionButton()), findsOneWidget);
+
+    // You can add more specific tests based on your app's structure and widgets.
+  });
 }
